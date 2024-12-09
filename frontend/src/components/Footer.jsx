e59cbf10dc90d46da949c202e288766c85ef70e7
Malik Touqeer <touqeeryameen@gmail.com>
2024-12-09 22:46:18 +0500

    commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <div className="banner">
         <div className="title">
-          <h1>EventGlow</h1>
+          <h1>Event</h1>
           <p>Events and Weddings</p>
         </div>
         <div className="tag">
